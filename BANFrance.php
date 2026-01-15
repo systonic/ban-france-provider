30aa3622c5cd507daf1cca16a210566fcc84d288
opi <opi@zeropi.net>
2026-01-15 09:32:43 +0100

    Update API Endpoint
    api-adresse.data.gouv.fr is deprecated and will by shut down at the end of January 2026.

--- BANFrance.php
+++ BANFrance.php
@@ -31,12 +31,12 @@ final class BANFrance extends AbstractHttpProvider implements Provider
     /**
      * @var string
      */
-    const GEOCODE_ENDPOINT_URL = 'https://api-adresse.data.gouv.fr/search/?q=%s&limit=%d';
+    const GEOCODE_ENDPOINT_URL = 'https://data.geopf.fr/geocodage/search/?q=%s&limit=%d';
 
     /**
      * @var string
      */
-    const REVERSE_ENDPOINT_URL = 'https://api-adresse.data.gouv.fr/reverse/?&lon=%F&lat=%F';
+    const REVERSE_ENDPOINT_URL = 'https://data.geopf.fr/geocodage/reverse/?&lon=%F&lat=%F';
 
     /**
      * @param ClientInterface $adapter An HTTP adapter.
